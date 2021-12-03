8950257dee691f919da438d42855b9d75cd848b2
EYE657318 <isaactphillips@gmail.com>
2021-12-02 23:06:00 -0500

    Should have everything

--- counts.cpp
+++ counts.cpp
@@ -8,10 +8,10 @@
 using namespace std;
 
 int countChar(string text){
-    cout << "Testing " << text << endl;
+    //cout << "Testing " << text << endl;
     int charcount = 0;
     for(int i = 0; i < text.size(); i++){
-        if(text.at(i) != ' '){
+        if(text.at(i) != '\n'){
             charcount++;
         }
     }
@@ -21,7 +21,7 @@ int countChar(string text){
 }
 
 int countLine(string text){
-    cout << "Testing " << text << endl;
+    //cout << "Testing " << text << endl;
     int linecount = 0;
     for(int i = 0; i < text.size(); i++){
         if(text.at(i) == '\n'){
@@ -34,14 +34,59 @@ int countLine(string text){
 }
 
 int main(){
+char a[10];
+a[10] = 0;
+
+
     string inputstring;
     string examstring;
 
     ifstream thefile;
+
+    ifstream test1;
+    ifstream test2;
+    ifstream test3;
     
 
     int charcount=0;
     int linecount=0; 
+
+    test1.open("test.txt");
+    if(test1.is_open()){
+        while(!test1.eof()){
+            examstring = test1.get();
+            charcount += countChar(examstring);
+        }
+    }
+    if(charcount == 13){
+        cout << "Test 1 passed" << endl;
+    }
+    charcount = 0;
+
+    test2.open("test2.txt");
+    if(test2.is_open()){
+        while(!test2.eof()){
+            examstring = test2.get();
+            charcount += countChar(examstring);
+        }
+    }
+    if(charcount == 142){
+        cout << "Test 2 passed" << endl;
+    }
+    charcount = 0;
+
+    test3.open("test3.txt");
+    if(test3.is_open()){
+        while(!test3.eof()){
+            examstring = test3.get();
+            charcount += countChar(examstring);
+        }
+    }
+    if(charcount == 16){
+        cout << "Test 3 passed" << endl;
+    }
+
+    charcount = 0;
 
     cin >> inputstring;
 
